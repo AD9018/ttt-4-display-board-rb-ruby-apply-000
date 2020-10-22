b24a0803464d8bba2405261ec2323771064638e7
Adam Dadi <adamdadi23@gmail.com>
2020-10-22 20:18:16 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -6,14 +6,3 @@ def display_board(board)
   puts "-----------"
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end 
-
-
-
-
-#Blake A Long 3 MINUTES AGO
-#You should be interpolating each board index in an individual cell 
-#those puts strings, correct
-
-
-# Define display_board that accepts a board and prints
-# out the current state.
